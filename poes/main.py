181bb46a748a70ddf910ee6922503f6c1f1753d0
FreddyEcu-Ch <fpcarrio@espol.edu.ec>
2024-10-30 06:10:30 -0500

    random values added of swi

--- poes/main.py
+++ poes/main.py
@@ -78,3 +78,31 @@ area_expon = np.where(area_expon > 500, 500, area_expon)
 # Random values for area using uniform distribution
 area_uni = uniform.rvs(loc=0, scale=500, size=1000)
 print(area_uni)
+
+
+#%% Generate random values for swi
+
+# Normal distribution
+swi_norm = norm.rvs(loc=0.4, scale=0.05, size=1000)
+swi_norm = np.clip(swi_norm, 0, 1)
+
+# Lognormal distribution
+swi_lognorm = lognorm.rvs(s=0.1, loc=0, scale=0.05, size=1000)
+swi_lognorm = np.clip(swi_lognorm, 0, 1)
+
+# Exponential distribution
+swi_expon = expon.rvs(loc=0, scale=0.05, size=1000)
+swi_expon = np.clip(swi_expon, 0, 1)
+
+# Triangular distribution
+swi_tri = triang.rvs(c=0.3, loc=0, scale=1, size=1000)
+
+# Uniform distribution
+swi_uni = uniform.rvs(loc=0, scale=1, size=1000)
+
+print("Valores generados para swi:")
+print("Normal:", swi_norm)
+print("Lognormal:", swi_lognorm)
+print("Exponencial:", swi_expon)
+print("Triangular:", swi_tri)
+print("Uniforme:", swi_uni)
