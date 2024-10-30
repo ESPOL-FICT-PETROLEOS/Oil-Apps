9eecef520a059aab126ca6efe0b3dedd855eb6a7
FreddyEcu-Ch <fpcarrio@espol.edu.ec>
2024-10-30 07:09:08 -0500

    End of POES-22.

--- poes/controller/controller.py
+++ poes/controller/controller.py
@@ -1,18 +1,41 @@
 import xlwings as xw
+from poes.model.poes import poes
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Define Sheets Names
+SUMMARY = "Summary"
+RESULTS = "Results"
+
+# Define Column Names
+VARIABLES = "Variables"
+VALUES = "Values"
+DISTRIBUTION = "Distribution"
+LOC = "Loc"
+SCALE = "Scale"
+C = "c"
+LIM_MIN = "Limit min"
+LIM_MAX = "Limit max"
+
+# Call cells from Ms Excel
+DET_VALUES = "det_values"
+DF_POES = "def_poes"
+REALIZATIONS = "realizations"
+SEED = "seed"
+
+# Write values in Excel from Python
+POES_DET = "det_result"
 
 
 def main():
     wb = xw.Book.caller()
-    sheet = wb.sheets[0]
-    if sheet["A1"].value == "Hello xlwings!":
-        sheet["A1"].value = "Bye xlwings!"
-    else:
-        sheet["A1"].value = "Hello xlwings!"
+    # Define sheet to use
+    sheet = wb.sheets[SUMMARY]
+    # Call values for deterministic POES
+    params = sheet[DET_VALUES].options(np.array, Transpose=True).value
 
-
-@xw.func
-def hello(name):
-    return f"Hello {name}!"
+    sheet[POES_DET].value = poes(*params)
 
 
 if __name__ == "__main__":
