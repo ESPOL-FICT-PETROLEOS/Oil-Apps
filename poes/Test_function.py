a42f05110b398d4fc64e6c9e4ec8c0173ca77fc6
FreddyEcu-Ch <fpcarrio@espol.edu.ec>
2024-11-06 07:22:14 -0500

    Update of this file.

--- poes/Test_function.py
+++ poes/Test_function.py
@@ -1,27 +1,36 @@
-#%% Import Python Libraries
+# %% Import Python Libraries
 import xlwings as xw
 import pandas as pd
 import numpy as np
 from poes.model.utils import param_stoiip
 
 
-#%% Call POES Workbook
+# %% Call POES Workbook
 wb = xw.Book("poes/controller/poes.xlsm")
-sheet = wb.sheets['Summary']
+sheet = wb.sheets["Summary"]
 
-#%%
+# %%
 # Call DataFrame from Ms. Excel
 df_poes = sheet["A2"].options(pd.DataFrame, expand="table", index=False).value
 
 
-#%% Test param_stoiip function
-area_stoc = param_stoiip(df_poes, 0, "Distribution", "Loc", "Scale", 100, "c", "Lim min", "Lim max")
+# %% Test param_stoiip function
+area_stoc = param_stoiip(
+    df_poes, 0, "Distribution", "Loc", "Scale", 100, "c", "Lim min", "Lim max"
+)
 
-h_stoc = param_stoiip(df_poes, 1, "Distribution", "Loc", "Scale", 100, "c", "Lim min", "Lim max")
+h_stoc = param_stoiip(
+    df_poes, 1, "Distribution", "Loc", "Scale", 100, "c", "Lim min", "Lim max"
+)
 
-porosity_stoc = param_stoiip(df_poes, 2, "Distribution", "Loc", "Scale", 100, "c", "Lim min", "Lim max")
+porosity_stoc = param_stoiip(
+    df_poes, 2, "Distribution", "Loc", "Scale", 100, "c", "Lim min", "Lim max"
+)
 
-swi_stoc = param_stoiip(df_poes, 3, "Distribution", "Loc", "Scale", 100, "c", "Lim min", "Lim max")
+swi_stoc = param_stoiip(
+    df_poes, 3, "Distribution", "Loc", "Scale", 100, "c", "Lim min", "Lim max"
+)
 
-boi_stoc = param_stoiip(df_poes, 4, "Distribution", "Loc", "Scale", 100, "c", "Lim min", "Lim max")
-
+boi_stoc = param_stoiip(
+    df_poes, 4, "Distribution", "Loc", "Scale", 100, "c", "Lim min", "Lim max"
+)
